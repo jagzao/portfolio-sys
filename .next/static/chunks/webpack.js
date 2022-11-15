58997ee265fb17760690e468b743d1fe0912ed23
jagza <g.zambrano@desdecero.digital>
2022-11-14 18:43:10 -0600

    Dropdown, icon, fadeNU

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "db290e74960034ff"; }
+/******/ 		__webpack_require__.h = function() { return "3509952288049f9e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
